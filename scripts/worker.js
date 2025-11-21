bd9a520ca38b98e7675d7e5614874e4abda9c7a1
flanrin <flanrin@outlook.com>
2025-11-21 21:51:23 +0800

    11

--- scripts/worker.js
+++ scripts/worker.js
@@ -5,18 +5,18 @@
 // Polyfills for Worker environment
 self.window = self;
 self.document = {
-    createElement: function() { return {}; } // Mock for safety
+    createElement: function () { return {}; } // Mock for safety
 };
 
 importScripts('../libs/lz-string.min.js', 'ErrorException.js', 'RPGFile.js', 'Decrypter.js');
 
 // Override createBlobUrl to avoid window.URL usage and just keep the content
-RPGFile.prototype.createBlobUrl = function(toNormal) {
+RPGFile.prototype.createBlobUrl = function (toNormal) {
     // Create the Blob to be sent back to the main thread
-    this.blob = new Blob([this.content], {type: this.getMimeType(toNormal)});
+    this.blob = new Blob([this.content], { type: this.getMimeType(toNormal) });
 };
 
-self.onmessage = function(e) {
+self.onmessage = function (e) {
     var data = e.data;
     var file = data.file;
     var mode = data.mode; // 'decrypt', 'encrypt', 'restore'
@@ -35,10 +35,17 @@ self.onmessage = function(e) {
         decrypter.remain = header.remain;
     }
 
-    var callback = function(rpgFile, err) {
+    var callback = function (rpgFile, err) {
         if (err) {
             self.postMessage({ status: 'error', error: err.toString(), fileName: file.name, relativePath: data.relativePath });
         } else {
+            // Rename file extension
+            if (mode === 'decrypt' || mode === 'restore') {
+                rpgFile.convertExtension(true);
+            } else if (mode === 'encrypt') {
+                rpgFile.convertExtension(false);
+            }
+
             // Send back the processed blob and the original file name/path info
             self.postMessage({
                 status: 'success',
@@ -51,14 +58,16 @@ self.onmessage = function(e) {
     };
 
     try {
+        console.log("working");
         if (mode === 'decrypt') {
             decrypter.decryptFile(rpgFile, callback);
         } else if (mode === 'encrypt') {
             decrypter.encryptFile(rpgFile, callback);
         } else if (mode === 'restore') {
+            console.log("process" + rpgFile);
             decrypter.restoreHeader(rpgFile, callback);
         } else {
-             self.postMessage({ status: 'error', error: 'Unknown mode: ' + mode, fileName: file.name });
+            self.postMessage({ status: 'error', error: 'Unknown mode: ' + mode, fileName: file.name });
         }
     } catch (err) {
         self.postMessage({ status: 'error', error: err.toString(), fileName: file.name, relativePath: data.relativePath });
